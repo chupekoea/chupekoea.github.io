03e2f7f5e913cbda5e374b0bbc4a100d4e996822
Evgeniy Chupeko <echupeko@softservice.org>
2025-11-19 22:43:24 +0700

    правка сборки

--- src/app/App.vue
+++ src/app/App.vue
@@ -59,7 +59,7 @@ const fetchApiText = async () => {
     const API_URL = import.meta.env.VITE_API_URL;
     const response = await fetch(`${API_URL}/api/text`)
     const data = await response.json()
-    // alert(data.message)
+    console.log(data.message)
   } catch (error) {
     // alert('Ошибка при получении данных: ' + error)
   }
